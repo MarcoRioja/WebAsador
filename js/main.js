28b2714b2e822c4fb6d7c184089e26a663bb9613
Marco <mercprv@gmail.com>
2022-11-09 12:56:17 +0100

    Up

---
+++ js/main.js
@@ -0,0 +1,111 @@
+//Modal 1
+
+const openModalM1 = document.querySelector('.modalM1');
+const modalM1 = document.querySelector('.modal__M1');
+const closeModalM1 = document.querySelector('.modal__closeM1');
+
+openModalM1.addEventListener('click', (e) =>{
+    e.preventDefault();
+    modalM1.classList.add('modal--show');
+})
+
+closeModalM1.addEventListener('click', (e) =>{
+    e.preventDefault();
+    modalM1.classList.remove('modal--show');
+})
+
+//Modal 1 C
+
+const openModalM1C = document.querySelector('.modalM1C');
+const modalM1C = document.querySelector('.modal__M1C');
+const closeModalM1C = document.querySelector('.modal__closeM1C');
+
+openModalM1C.addEventListener('click', (e) =>{
+    e.preventDefault();
+    modalM1C.classList.add('modal--show');
+})
+
+closeModalM1C.addEventListener('click', (e) =>{
+    e.preventDefault();
+    modalM1C.classList.remove('modal--show');
+})
+
+//Modal 2
+
+const openModalM2 = document.querySelector('.modalM2');
+const modalM2 = document.querySelector('.modal__M2');
+const closeModalM2 = document.querySelector('.modal__closeM2');
+
+openModalM2.addEventListener('click', (e) =>{
+    e.preventDefault();
+    modalM2.classList.add('modal--show');
+})
+
+closeModalM2.addEventListener('click', (e) =>{
+    e.preventDefault();
+    modalM2.classList.remove('modal--show');
+})
+
+//Modal 2 C
+
+const openModalM2C = document.querySelector('.modalM2C');
+const modalM2C = document.querySelector('.modal__M2C');
+const closeModalM2C = document.querySelector('.modal__closeM2C');
+
+openModalM2C.addEventListener('click', (e) =>{
+    e.preventDefault();
+    modalM2C.classList.add('modal--show');
+})
+
+closeModalM2C.addEventListener('click', (e) =>{
+    e.preventDefault();
+    modalM2C.classList.remove('modal--show');
+})
+
+//Modal 3
+
+const openModalM3 = document.querySelector('.modalM3');
+const modalM3 = document.querySelector('.modal__M3');
+const closeModalM3 = document.querySelector('.modal__closeM3');
+
+openModalM3.addEventListener('click', (e) =>{
+    e.preventDefault();
+    modalM3.classList.add('modal--show');
+})
+
+closeModalM3.addEventListener('click', (e) =>{
+    e.preventDefault();
+    modalM3.classList.remove('modal--show');
+})
+
+//Modal 4
+
+const openModalM4 = document.querySelector('.modalM4');
+const modalM4 = document.querySelector('.modal__M4');
+const closeModalM4 = document.querySelector('.modal__closeM4');
+
+openModalM4.addEventListener('click', (e) =>{
+    e.preventDefault();
+    modalM4.classList.add('modal--show');
+})
+
+closeModalM4.addEventListener('click', (e) =>{
+    e.preventDefault();
+    modalM4.classList.remove('modal--show');
+})
+
+//Modal 5
+
+const openModalM5 = document.querySelector('.modalM5');
+const modalM5 = document.querySelector('.modal__M5');
+const closeModalM5 = document.querySelector('.modal__closeM5');
+
+openModalM5.addEventListener('click', (e) =>{
+    e.preventDefault();
+    modalM5.classList.add('modal--show');
+})
+
+closeModalM5.addEventListener('click', (e) =>{
+    e.preventDefault();
+    modalM5.classList.remove('modal--show');
+})
